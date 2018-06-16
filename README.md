08d47c34f264f287e7993d0425145a37d34d5faf
Patryk Knapik <226302@student.pwr.edu.pl>
2018-06-16 23:17:24 +0200

    Tabu added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # README
 Brute force algorithm comparison to Johnson's algorithm for solving two and three machines flowshop problem.
-NEH algorithm (O(n^2)) for three machines flowshop problem.
+NEH algorithm (O(n^2)) and Tabu algorithm for three machines flowshop problem.
 
 Tasks are created in loop. Values of Mx (where x is machine number) are generated randomly,
 they represent time needed for x machine to process task.
@@ -10,9 +10,10 @@ You may want to change:
 1. Number of tasks by changing `TASK_COUNT`
 2. Minimal time for machine by changing `MIN_TIME`
 3. Maximal time for machine by changing `MAX_TIME`
-4. You can save some time by turning off `SHOW_PERMUTATIONS`
-5. To `GENERATE_PLOT` You will need `gnuplot`
-6. I used `GENERATE_TEST_DATA` to generate tables below
+4. Tabu algorithm run time by changing `TABOO_TIME`
+5. You can save some time by turning off `SHOW_PERMUTATIONS`
+6. To `GENERATE_PLOT` You will need `gnuplot`
+7. I used `GENERATE_TEST_DATA` to generate tables below
 
 ### Test times
 
@@ -32,19 +33,18 @@ You may want to change:
 |12|0.014 ms|123|198605 ms|123|
 
 #### Three Machines
-| Number of tasks: |Johnson's algorithm | make span | Brute force | make span| NEH algorithm | make span|
-| ------------- |-------------:| -----:| -----:| -----:|----:|----:|
-|2|0.007 ms|34|0.007 ms|34|0.014 ms|34|
-|3|0.008 ms|57|0.009 ms|55|0.012 ms|55|
-|4|0.007 ms|54|0.012 ms|54|0.012 ms|54|
-|5|0.009 ms|66|0.055 ms|63|0.02 ms|64|
-|6|0.011 ms|72|0.326 ms|67|0.026 ms|68|
-|7|0.008 ms|97|1.854 ms|84|0.024 ms|84|
-|8|0.009 ms|88|16.771 ms|88|0.029 ms|88|
-|9|0.016 ms|123|290.34 ms|116|0.053 ms|116|
-|10|0.016 ms|115|4221.17 ms|111|0.059 ms|111|
-|11|0.011 ms|137|31826.8 ms|128|0.05 ms|128|
-|12|0.011 ms|174|266816 ms|160|0.057 ms|160|
+| Number of tasks: |Johnson's algorithm | make span | Brute force | make span| NEH algorithm | make span| Tabu algorithm | make span |
+| ------------- |-------------:| -----:| -----:| -----:|----:|----:|----:|----:|
+|3|0.007 ms|53|0.004 ms|53|0.009 ms|53|20675.4 ms|53|
+|4|0.007 ms|87|0.01 ms|81|0.012 ms|81|20420.4 ms|81|
+|5|0.007 ms|63|0.04 ms|63|0.015 ms|67|20606.8 ms|64|
+|6|0.014 ms|79|0.404 ms|71|0.027 ms|71|20023.7 ms|71|
+|7|0.064 ms|85|2.285 ms|85|0.028 ms|85|20623.3 ms|85|
+|8|0.011 ms|112|20.08 ms|112|0.04 ms|112|20790.6 ms|112|
+|9|0.013 ms|153|270.539 ms|153|0.045 ms|153|20963.2 ms|156|
+|10|0.015 ms|128|3593.9 ms|127|0.064 ms|127|20950.6 ms|128|
+|11|0.014 ms|131|25595.4 ms|128|0.076 ms|128|20203.1 ms|128|
+|12|0.049 ms|136|269797 ms|136|0.082 ms|136|20069.9 ms|136|
 
 ### Visualisation
 I'm using `gnuplot` to visualise chosen task order.
@@ -57,5 +57,7 @@ Johnson's algorithm is significantly faster than brute force and gives the same 
 Three machines version of Johnson's algorithm is not perfect but it is close enough for the time savings.
 Three machines NEH algorithm gives almost ideal results (most of the time, they are the same as brute force) 
 in a time comparable to Johnson's algorithm.
+Tabu algorithm gives results comparable to NEH and algorithm and mostly better than Johnson's algorithm.
+This implementation of Tabu algorithm is very simple and time based.
 
 *for SPD lab pt11 on PWR*
--- main.cpp
+++ main.cpp
@@ -5,12 +5,13 @@
 #include "twoMachines.h"
 #include "threeMachines.h"
 
-#define TASK_COUNT 5
+#define TASK_COUNT 12
 #define MIN_TIME 1
 #define MAX_TIME 20
+#define TABOO_TIME 20
 #define SHOW_PERMUTATIONS false
-#define GENERATE_PLOT true
-#define GENERATE_TEST_DATA false
+#define GENERATE_PLOT false
+#define GENERATE_TEST_DATA true
 
 unsigned long int factorial(unsigned long int i) {
     unsigned long int f = 1;
@@ -21,11 +22,56 @@ unsigned long int factorial(unsigned long int i) {
     return f;
 }
 
+bool isTimeElapsed(timeval startTime, timeval currentTime, double period) {
+    double tmp = currentTime.tv_sec - startTime.tv_sec;
+    return tmp > period;
+
+}
+
+std::vector<std::vector<task *> > generateNeigbouhood(std::vector<task *> tasks, unsigned int iter) {
+    std::vector<std::vector<task *> > tmpList;
+    task *tmptask = tasks.at(iter);
+    tasks.erase(tasks.begin() + iter);
+    std::vector<task *> backup = tasks;
+    for (auto i = 0; i < tasks.size(); ++i) {
+        if (i == iter)
+            continue;
+        tasks.insert(tasks.begin() + i, tmptask);
+        tmpList.push_back(tasks);
+        tasks = backup;
+
+    }
+
+    return tmpList;
+}
+
+unsigned int getMakeSpanThreeMs(const std::vector<task *> &tasks) {
+    unsigned int M1time = 0, M2time = 0, M3time = 0, Cmax = 0;
+    for (auto iter = tasks.begin(); iter != tasks.end(); ++iter) {
+        if (iter == tasks.begin()) {
+            M2time = M1time = M3time = (*iter)->getM1();
+            M2time += (*iter)->getM2();
+            M3time += (*iter)->getM2() + (*iter)->getM3();
+        } else {
+            M1time += (*iter)->getM1();
+            M2time = std::max(M2time, M1time) + (*iter)->getM2();
+            M3time = std::max(M2time, M3time) + (*iter)->getM3();
+        }
+
+    }
+
+    Cmax = std::max(M1time, M2time);
+    Cmax = std::max(M3time, Cmax);
+
+    return Cmax;
+}
+
 
 int main() {
-    timeval time_stamp_start, time_stamp_stop;
-    double elapsed_time, twoMachinesBruteForceTime, twoMachinesJohnsonTime, threeMachinesBruteForceTime, threeMachinesJohnsonTime, threeMachinesNEHTime;
-    std::vector<task *> tasks;
+    timeval time_stamp_start, time_stamp_stop, time_stamp_taboo;
+    double elapsed_time, twoMachinesBruteForceTime, twoMachinesJohnsonTime, threeMachinesBruteForceTime, threeMachinesJohnsonTime, threeMachinesNEHTime, threeMachinesTabuTime;
+    std::vector<task *> tasks, tabuBestTasks, tabuBestTasksLocal, tabuBestTasksLocalTmp;
+    std::vector<std::vector<task *> > tabuList, tabuNeighbourhood;
     std::list<task *> johnsonsTasksFront, johnsonsTasksBack;
     unsigned long int tasksPermutations = 0;
     std::srand(std::time(nullptr));
@@ -207,6 +253,90 @@ int main() {
 
     threeMachinesNEHTime = elapsed_time;
 
+
+    std::sort(tasks.begin(), tasks.end(), [](const task *a, const task *b) -> bool { return a->getID() < b->getID(); });
+    std::cout << "Tasks order reset before tabu: ";
+    for (task *Task : tasks) {
+        std::cout << Task->getID() << ' ';
+    }
+
+    //tabu
+    unsigned iter = 0, localCmaxTmp, localCmax = std::numeric_limits<unsigned>::max();
+    bool tabuEnd = false, tabuListFlag = false;
+    std::cout << "\n\nTabu algorithm:\n";
+    gettimeofday(&time_stamp_start, nullptr);
+    time_stamp_taboo = time_stamp_start;
+    tabuBestTasksLocal = tabuBestTasks = tasks;
+    while (!tabuEnd) {
+
+        tabuNeighbourhood = generateNeigbouhood(tabuBestTasksLocal, iter);
+        if (tabuNeighbourhood.size() == 0) {
+            iter = 0;
+            for (std::vector<task *> tmpTasks : tabuList) {
+                localCmaxTmp = getMakeSpanThreeMs(tmpTasks);
+                if (localCmaxTmp < localCmax) {
+                    localCmax = localCmaxTmp;
+                    tasks = tmpTasks;
+                }
+            }
+            localCmax = std::numeric_limits<unsigned>::max();
+            gettimeofday(&time_stamp_taboo, nullptr);
+            if (isTimeElapsed(time_stamp_start, time_stamp_taboo, TABOO_TIME)) {
+                tabuEnd = true;
+            }
+            continue;
+        }
+        for (std::vector<task *> tmpTasks : tabuNeighbourhood) {
+
+            if (std::find(tabuList.begin(), tabuList.end(), tmpTasks) != tabuList.end()) {
+                continue;
+            }
+            localCmaxTmp = getMakeSpanThreeMs(tmpTasks);
+            if (localCmaxTmp < localCmax) {
+                localCmax = localCmaxTmp;
+                tabuBestTasksLocalTmp = tmpTasks;
+            }
+        }
+        localCmax = std::numeric_limits<unsigned>::max();
+        //time elapsed
+        gettimeofday(&time_stamp_taboo, nullptr);
+        if (isTimeElapsed(time_stamp_start, time_stamp_taboo, TABOO_TIME)) {
+            tabuEnd = true;
+            continue;
+        }
+        if (getMakeSpanThreeMs(tabuBestTasksLocalTmp) < getMakeSpanThreeMs(tabuBestTasksLocal)) {
+            tabuBestTasksLocal = tabuBestTasksLocalTmp;
+            tabuList.push_back(tabuBestTasksLocal);
+            iter = 0;
+        } else {
+            ++iter;
+        }
+
+        if (iter >= tasks.size()) {
+            iter = 0;
+            for (std::vector<task *> tmpTasks : tabuList) {
+                localCmaxTmp = getMakeSpanThreeMs(tmpTasks);
+                if (localCmaxTmp < localCmax) {
+                    localCmax = localCmaxTmp;
+                    tasks = tmpTasks;
+                }
+            }
+            localCmax = std::numeric_limits<unsigned>::max();
+        }
+    }
+    gettimeofday(&time_stamp_stop, nullptr);
+
+    std::cout << "Sugested tasks queue for three machines: ";
+    for (task *Task : tasks) {
+        std::cout << Task->getID() << ' ';
+    }
+    std::cout << "; Cmin = " << getMakeSpanThreeMs(tasks) << '\n';
+    elapsed_time = (time_stamp_stop.tv_sec - time_stamp_start.tv_sec) * 1000.0;      // sec to ms
+    elapsed_time += (time_stamp_stop.tv_usec - time_stamp_start.tv_usec) / 1000.0;   // us to ms
+    std::cout << "Computation time: " << elapsed_time << " ms.\n\n\n";
+
+    threeMachinesTabuTime = elapsed_time;
+
     if (GENERATE_PLOT) {
         std::string nazwa("bfplot2.svg");
 
@@ -229,8 +359,10 @@ int main() {
         file.open("test_data_three_machines.log", std::ofstream::app);
         file << "|" << TASK_COUNT << "|" << threeMachinesJohnsonTime << " ms|" << threeMsJohnson.getMinMakeSpan();
         file << "|" << threeMachinesBruteForceTime << " ms|" << threeMs.getMinMakeSpan();
-        file << "|" << threeMachinesNEHTime << " ms|" << threeMsNEH.getMinMakeSpan() << "|\n";
+        file << "|" << threeMachinesNEHTime << " ms|" << threeMsNEH.getMinMakeSpan();
+        file << "|" << threeMachinesTabuTime << " ms|" << getMakeSpanThreeMs(tasks) << "|\n";
         file.close();
+
     }
 
 
